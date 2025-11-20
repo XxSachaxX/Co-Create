0d9ac4cca446617ddbf16e43d2ee763b7a58c1b0
XxSachaxX <sacha@gdpr.dev>
2025-11-20 16:36:23 +0100

    feature(users): add name to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_11_20_152000) do
+ActiveRecord::Schema[8.1].define(version: 2025_11_20_153539) do
   create_table "projects", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "description"
@@ -32,6 +32,7 @@ ActiveRecord::Schema[8.1].define(version: 2025_11_20_152000) do
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "email_address", null: false
+    t.string "name"
     t.string "password_digest", null: false
     t.datetime "updated_at", null: false
     t.index ["email_address"], name: "index_users_on_email_address", unique: true
