9cf1fd5112417c55785ee5a80e353d3e5bc9e5ab
XxSachaxX <sacha@gdpr.dev>
2025-11-20 17:48:27 +0100

    feature(user): Add user name to sign up form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,6 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:email_address, :password)
+    params.require(:user).permit(:email_address, :password, :name)
   end
 end
