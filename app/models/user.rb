96ac73a19f0eb990d0a795caced6a9fd0d0a8030
XxSachaxX <sacha@gdpr.dev>
2025-11-20 17:48:03 +0100

    Correct indentation issues in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
-include Uuidable
-has_secure_password
+  include Uuidable
+  has_secure_password
 
-has_many :sessions, dependent: :destroy
-has_many :projects, dependent: :destroy
+  has_many :sessions, dependent: :destroy
+  has_many :projects, dependent: :destroy
 
   normalizes :email_address, with: ->(e) { e.strip.downcase }
 end
