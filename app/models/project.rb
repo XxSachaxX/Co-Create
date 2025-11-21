11a3bd6674ab7d0462a59fc54509ec892431bf98
XxSachaxX <sacha@gdpr.dev>
2025-11-21 11:36:04 +0100

    feature(projects): add validations on projects fields

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,6 @@
 class Project < ApplicationRecord
   include Uuidable
   belongs_to :user, required: true
+  validates :description, presence: true, length: { minimum: 50 }
+  validates :name, presence: true
 end
---
+++ spec/models/projects_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe Project, type: :model do
+  describe ".create" do
+    let(:user) { FactoryBot.create(:user) }
+
+    describe "when the description is too short" do
+      it "is not valid" do
+        expect{ Project.create!(description: "A short description", user_id: user.id, name: "Project Name") }.to raise_error(ActiveRecord::RecordInvalid)
+      end
+    end
+
+    describe "when there is no description" do
+      it "is not valid" do
+        expect{ Project.create!(user_id: user.id, name: "Project Name") }.to raise_error(ActiveRecord::RecordInvalid)
+      end
+    end
+
+    describe "when the description is longer than 50 characters" do
+      it "is valid" do
+        expect { Project.create!(description: "A very long description that exceeds the maximum length", user_id: user.id, name: "Project Name") }.to change { Project.count }.by(1)
+      end
+    end
+
+    describe "when there is no project name" do
+      it "is not valid" do
+        expect { Project.create!(description: "A very long description that exceeds the maximum length", user_id: user.id) }.to raise_error(ActiveRecord::RecordInvalid)
+      end
+    end
+  end
+end
