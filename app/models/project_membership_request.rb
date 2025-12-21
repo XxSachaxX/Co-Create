b17f6151aa1c36a2b8c7dda7da51b073627ac89a
XxSachaxX <sacha@gdpr.dev>
2025-12-21 17:33:23 +0100

    feat(project-membership-requests): - Display pending project memberships in the project - Add placeholder links to accept or request request

--- app/models/project_membership_request.rb
+++ app/models/project_membership_request.rb
@@ -1,5 +1,6 @@
 class ProjectMembershipRequest < ApplicationRecord
   include Uuidable
+  scope :pending, -> { where(status: ProjectMembership::PENDING) }
 
   belongs_to :project
   belongs_to :user
