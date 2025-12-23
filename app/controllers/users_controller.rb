4040d2787c29bffda303ddca00ab7bed71955f0b
XxSachaxX <sacha@gdpr.dev>
2025-12-23 20:13:16 +0100

    feat(internationalization): - Extract notice from UsersController to en.yml - Replace usage

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to root_path, notice: "User was successfully created."
+      redirect_to root_path, notice: I18n.t("users.account_created")
     else
       render :new
     end
