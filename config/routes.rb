d0b0fb9625f0fb592ec8b66987820ff1bc7a713e
XxSachaxX <sacha@gdpr.dev>
2026-01-03 13:50:06 +0100

    feat(footer): - Create a new footer - Create placeholder privacy policies and terms of service pages

--- config/routes.rb
+++ config/routes.rb
@@ -25,6 +25,11 @@ Rails.application.routes.draw do
     end
   end
   resources :passwords, param: :token
+
+  # Static pages
+  get "terms", to: "static_pages#terms", as: :terms
+  get "privacy", to: "static_pages#privacy", as: :privacy
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
