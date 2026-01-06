29102a5c09817abb077ddd0b7b637aa86193a398
XxSachaxX <sacha@gdpr.dev>
2026-01-06 16:31:00 +0100

    feat(profile): Create a profile by default on User creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,12 @@ class User < ApplicationRecord
   has_one :profile, dependent: :destroy
 
   normalizes :email_address, with: ->(e) { e.strip.downcase }
+
+  after_create :create_default_profile
+
+  private
+
+  def create_default_profile
+    create_profile!(description: "")
+  end
 end
